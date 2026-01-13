3c486484a58d92e3143f16f1cccf001a0cb60a56
Anupama <anupama200603@gmail.com>
2026-01-13 14:32:46 +0530

    Log VITE environment object to console

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+console.log("VITE ENV OBJECT:", import.meta.env);
+
 createRoot(document.getElementById("root")!).render(<App />);
